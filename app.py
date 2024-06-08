dc17e6706d3f83d9cc0ff7e5b7376d4c9bc25cc6
Prveen <praveenteeshu@gmail.com>
2024-06-08 18:06:48 +0530

    Data Ingestion

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from src.mlpathlearn.logger import logging
 from src.mlpathlearn.exception import CustomException
+from src.mlpathlearn.components.data_ingestion import DataIngestion
+from src.mlpathlearn.components.data_ingestion import DataIngestionConfig
 import sys
 
 
@@ -9,7 +11,10 @@ if __name__=="__main__":
     logging.info("The excution started")
     
     try:
-        a=1/0
+        ##data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+   
     except Exception as e:
         logging.info("custom Exception")
         raise CustomException(e,sys)
